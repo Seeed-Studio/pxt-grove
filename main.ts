39d207f6d0e238ebf6d8751996c62e1f28d75ddb
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2021-12-27 10:07:34 +0100

    changed tx/rx

--- main.ts
+++ main.ts
@@ -706,8 +706,8 @@ namespace grove {
      */
     //% block="Setup Wifi|TX %txPin|RX %rxPin|Baud rate %baudrate|SSID = %ssid|Password = %passwd"
     //% group="UartWiFi"
-    //% txPin.defl=SerialPin.C16
-    //% rxPin.defl=SerialPin.C17
+    //% txPin.defl=SerialPin.C17
+    //% rxPin.defl=SerialPin.C16
     //% baudRate.defl=BaudRate.BaudRate115200
     export function setupWifi(txPin: SerialPin, rxPin: SerialPin, baudRate: BaudRate, ssid: string, passwd: string) {
         let result = 0
